1fdfdb78b4c9361ea2f53a14fee57036a8ecadac
George <zhoreeq@users.noreply.github.com>
2020-04-28 08:33:43 -0400

    autopeer flag

--- cmd/popura/main.go
+++ cmd/popura/main.go
@@ -94,6 +94,7 @@ func run_yggdrasil() {
 	normaliseconf := flag.Bool("normaliseconf", false, "use in combination with either -useconf or -useconffile, outputs your configuration normalised")
 	confjson := flag.Bool("json", false, "print configuration from -genconf or -normaliseconf as JSON instead of HJSON")
 	autoconf := flag.Bool("autoconf", false, "automatic mode (dynamic IP, peer with IPv6 neighbors)")
+	autopeer := flag.Bool("autopeer", false, "automatic Internet peering (using peers from github.com/yggdrasil-network/public-peers)")
 	ver := flag.Bool("version", false, "prints the version of this build")
 	logto := flag.String("logto", "stdout", "file path to log to, \"syslog\" or \"stdout\"")
 	getaddr := flag.Bool("address", false, "returns the IPv6 address as derived from the supplied configuration")
@@ -253,7 +254,7 @@ func run_yggdrasil() {
 	defer n.shutdown()
 
 	// Setup auto peering
-	if len(yggConfig.Peers) == 0 {
+	if *autopeer && len(yggConfig.Peers) == 0 {
 		for _, p := range getAutoPeers() {
 			if err := n.core.AddPeer(p, ""); err != nil {
 				logger.Infoln("Failed to connect to peer:", err)
